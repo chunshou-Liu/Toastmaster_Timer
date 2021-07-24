c2bfaaf1b58ef43d4d623a3126e882fbd9550acd
ChunShou-Liu <chunshou.iim07g@nctu.edu.tw>
2021-07-24 17:12:30 +0800

    set disable/enable function to startbutton and reformat code

--- js/timer.js
+++ js/timer.js
@@ -1,62 +1,76 @@
 var t;
 var seconds = 1;
-var gtime =  5 * 60;
-var ytime =  6 * 60;
-var rtime =  7.5 * 60;
-function setTabletimer(){
-    gtime =  1 * 60;
-    ytime =  1.5 * 60;
-    rtime =  2.5 * 60;
+var gtime = 5 * 60;
+var ytime = 6 * 60;
+var rtime = 7.5 * 60;
+
+function set_timer() {
+    var selectedOption = $("input:radio[name=timer_type]:checked").val();
+    switch (selectedOption) {
+        case 'Table topic(1-2)':
+            gtime = 1 * 60;
+            ytime = 1.5 * 60;
+            rtime = 2.5 * 60;
+            break;
+        case 'Individual Evaluator(2-3)':
+            gtime = 2 * 60;
+            ytime = 2.5 * 60;
+            rtime = 3.5 * 60;
+            break;
+        case 'Ice breaker(4-6)':
+            gtime = 4 * 60;
+            ytime = 5 * 60;
+            rtime = 6.5 * 60;
+            break;
+        case 'Prepared Speech(5-7)':
+            gtime = 5 * 60;
+            ytime = 6 * 60;
+            rtime = 7.5 * 60;
+            break;
+        default:
+
+    }
+
 }
-function setIndividualtimer(){
-    gtime =  2 * 60;
-    ytime =  2.5 * 60;
-    rtime =  3.5 * 60;
-}
-function setIcetimer(){
-    gtime =  4 * 60;
-    ytime =  5 * 60;
-    rtime =  6.5 * 60;
-}
-function setPreparedtimer(){
-    gtime =  5 * 60;
-    ytime =  6 * 60;
-    rtime =  7.5 * 60;
-}
+
 function changeColor() {
     if (seconds <= ytime && seconds >= gtime) {
-    document.body.style.background = "green";
-    }
-    else if (seconds <= rtime && seconds >= ytime) {
-    document.body.style.background = "yellow";
-    }
-    else if (seconds >= rtime){
+        document.body.style.background = "green";
+    } else if (seconds <= rtime && seconds >= ytime) {
+        document.body.style.background = "yellow";
+    } else if (seconds >= rtime) {
         document.body.style.background = "red";
-    }
-    else{
+    } else {
         document.body.style.background = "white";
     }
 }
+
 function secondPassed() {
-      var minutes = Math.round((seconds - 30) / 60);
-      if (seconds %60 <10){$("#stopWatch").html(minutes +":0" + seconds %60 );}
-      else{$("#stopWatch").html(minutes +":" + seconds %60 );}
-}
-function timer(){
-  secondPassed();
-  changeColor();
-      seconds++;
-      t = setTimeout("timer()", 1000);
+    var minutes = Math.round((seconds - 30) / 60);
+    if (seconds % 60 < 10) {
+        $("#stopWatch").html(minutes + ":0" + seconds % 60);
+    } else {
+        $("#stopWatch").html(minutes + ":" + seconds % 60);
+    }
 }
 
-function reset(){
+function timer() {
+    secondPassed();
+    changeColor();
+    seconds++;
+    t = setTimeout("timer()", 1000);
+}
+
+function reset() {
     seconds = 0;
-  changeColor();
-  clearTimeout(t);
-  secondPassed();
+    changeColor();
+    clearTimeout(t);
+    secondPassed();
+    document.getElementById("startButton").disabled = false;
 }
 
-function stop(){
+function stop() {
     clearTimeout(t);
+    document.getElementById("startButton").disabled = false;
 }
 
